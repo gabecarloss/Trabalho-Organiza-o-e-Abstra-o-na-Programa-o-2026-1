1666a8f3f2fe87626160c390d4979f53ea2c616c
gabecarloss <gabicarlos2006@gmail.com>
2026-03-14 22:05:48 -0300

    Add files via upload

---
+++ sistema_estoque/Sistema/venda.py
@@ -0,0 +1,8 @@
+class Venda:
+
+    def __init__(self, id, cliente_id, produto_id, quantidade, valor_total):
+        self.id = id
+        self.cliente_id = cliente_id
+        self.produto_id = produto_id
+        self.quantidade = quantidade
+        self.valor_total = valor_total
